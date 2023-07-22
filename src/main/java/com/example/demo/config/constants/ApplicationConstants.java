d119af2dfef815db8f501d9e2b805e8ad92f9cfd
Junho <chojunho433@gmail.com>
2023-07-22 23:23:35 +0900

    상수 클래스 final 선언 AccessLevel Private 추가

--- src/main/java/com/example/demo/config/constants/ApplicationConstants.java
+++ src/main/java/com/example/demo/config/constants/ApplicationConstants.java
@@ -3,7 +3,7 @@ package com.example.demo.config.constants;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ApplicationConstants {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ApplicationConstants {
     public static final String APPLICATION_NAME = "PRACTICE-JAVA-SPRING-BOOT";
 }
