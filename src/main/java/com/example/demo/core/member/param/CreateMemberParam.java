8eed7d70dd2e426193fb29432d46392fb93b1286
Junho <chojunho433@gmail.com>
2023-07-22 20:21:03 +0900

    CreateMemberParam 추가

---
+++ src/main/java/com/example/demo/core/member/param/CreateMemberParam.java
@@ -0,0 +1,11 @@
+package com.example.demo.core.member.param;
+
+import lombok.Getter;
+
+@Getter
+public class CreateMemberParam {
+
+    private String memberId;
+
+    private String memberName;
+}
