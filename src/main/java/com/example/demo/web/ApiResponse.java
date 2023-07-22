21727581579594f671bd725ababa4bd49af69a5c
Junho <chojunho433@gmail.com>
2023-07-22 23:35:04 +0900

    api 공통 response dto 추가

---
+++ src/main/java/com/example/demo/web/ApiResponse.java
@@ -0,0 +1,48 @@
+package com.example.demo.web;
+
+import lombok.NoArgsConstructor;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+@NoArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+
+    private String code;
+
+    private String message;
+
+    private T data;
+
+    public ApiResponse(Boolean success, String code, String message) {
+        this.success = success;
+        this.code = code;
+        this.message = message;
+    }
+
+    public ApiResponse(Boolean success, String code, String message, T data) {
+        this(success, code, message);
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiResponse{" +
+            "success=" + success +
+            ", code='" + code + '\'' +
+            ", message='" + message + '\'' +
+            ", data=" + data +
+            '}';
+    }
+
+    public static ApiResponse<Void> fail(String code, String message) {
+        return new ApiResponse<>(FALSE, code, message, null);
+    }
+
+    public static <D> ApiResponse<D> success(String code, String message, D data) {
+        return new ApiResponse<>(TRUE, code, message, data);
+    }
+}
+
