0b097356121eb3627b3d240939747c58f669c96f
Junho <chojunho433@gmail.com>
2023-07-22 21:03:28 +0900

    회원조회 서비스 클래스 추가

---
+++ src/main/java/com/example/demo/core/member/service/FindMemberService.java
@@ -0,0 +1,25 @@
+package com.example.demo.core.member.service;
+
+import com.example.demo.core.member.domain.Member;
+import com.example.demo.core.member.result.FindMemberResult;
+import com.example.demo.infrastructure.persistence.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class FindMemberService {
+
+    private final MemberRepository memberRepository;
+
+    public FindMemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public FindMemberResult findByMemberId(String memberId) {
+        Member member = memberRepository.findByMemberId(memberId)
+            .orElseThrow(IllegalArgumentException::new);
+
+        return FindMemberResult.from(member);
+    }
+}
