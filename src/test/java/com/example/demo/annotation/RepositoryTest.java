0aaee2e593f1ce7f1eb7e2c5505f36b02dde2132
Junho <chojunho433@gmail.com>
2023-07-22 23:17:23 +0900

    테스트 어노테이션 추가

---
+++ src/test/java/com/example/demo/annotation/RepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.annotation;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@DataJpaTest
+@TestEnvironment
+public @interface RepositoryTest {
+}
