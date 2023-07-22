59247047e89f1109a738f24642aa88b5a5046ed0
Junho <chojunho433@gmail.com>
2023-07-22 23:20:03 +0900

    member 조회 result dto에 createdAt 추가

--- src/main/java/com/example/demo/core/member/result/FindMemberResult.java
+++ src/main/java/com/example/demo/core/member/result/FindMemberResult.java
@@ -4,6 +4,8 @@ import com.example.demo.core.member.domain.Member;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class FindMemberResult {
@@ -14,11 +16,14 @@ public class FindMemberResult {
 
     private String memberName;
 
+    private LocalDateTime createdAt;
+
     public static FindMemberResult from(Member member) {
         return FindMemberResult.builder()
             .memberNo(member.getMemberNo())
             .memberId(member.getMemberId())
             .memberName(member.getMemberName())
+            .createdAt(member.getCreatedAt())
             .build();
     }
 }
