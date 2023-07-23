5fc79113b4a73785de11a2ca99a2df8dd29202b4
Junho <chojunho433@gmail.com>
2023-07-23 10:16:51 +0900

    Order, OrderProduct 엔티티 추가

---
+++ src/main/java/com/example/demo/core/order/domain/Order.java
@@ -0,0 +1,41 @@
+package com.example.demo.core.order.domain;
+
+import com.example.demo.config.persistence.AuditEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "orders")
+public class Order extends AuditEntity {
+    @Id
+    @Column(name = "order_no")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderNo;
+
+    @Column(name = "member_no", nullable = false, updatable = false)
+    private long memberNo;
+
+    @Column(name = "order_name", nullable = false, updatable = false)
+    private String orderName;
+
+    @Column(name = "transaction_amount", nullable = false, updatable = false)
+    private long transactionAmount;
+
+    @OneToMany(cascade = {CascadeType.PERSIST})
+    @JoinColumn(name = "order_product_id")
+    private Set<OrderProduct> products;
+}
---
+++ src/main/java/com/example/demo/core/order/domain/OrderProduct.java
@@ -0,0 +1,35 @@
+package com.example.demo.core.order.domain;
+
+import com.example.demo.config.persistence.AuditEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "order_product")
+public class OrderProduct extends AuditEntity {
+    @Id
+    @Column(name = "order_product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderProductId;
+
+    @Column(name = "product_code", updatable = false, nullable = false)
+    private String productCode;
+
+    @Column(name = "product_name", updatable = false, nullable = false)
+    private String productName;
+
+    @Column(name = "quantity", updatable = false, nullable = false)
+    private long quantity;
+
+    @Column(name = "product_amount", updatable = false, nullable = false)
+    private long productAmount;
+}
