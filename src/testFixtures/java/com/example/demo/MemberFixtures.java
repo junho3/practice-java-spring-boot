37d183fca360ab1983436cb8731c211c2f5dbeeb
Junho <chojunho433@gmail.com>
2023-07-23 11:50:24 +0900

    Fixtures 데이터 추가

--- src/testFixtures/java/com/example/demo/MemberFixtures.java
+++ src/testFixtures/java/com/example/demo/MemberFixtures.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 public class MemberFixtures {
+    private MemberFixtures() {}
 
+    public static final long MEMBER_NO = 723;
     public static final String MEMBER_ID = "banana";
     public static final String MEMBER_NAME = "춘식이";
 }
