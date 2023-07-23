a0bdff7df53627c46cc2321e33446cff4cdc28e3
Junho <chojunho433@gmail.com>
2023-07-23 12:25:37 +0900

    주문번호 조건 조회 추가

--- src/main/java/com/example/demo/infrastructure/persistence/order/OrderRepository.java
+++ src/main/java/com/example/demo/infrastructure/persistence/order/OrderRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.infrastructure.persistence.order;
 import com.example.demo.core.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findByOrderNo(String orderNo);
 }
