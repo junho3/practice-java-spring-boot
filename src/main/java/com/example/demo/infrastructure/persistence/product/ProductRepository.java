cda5a97c64c0e5236dc226d0f087ccfc2c3330f8
Junho <chojunho433@gmail.com>
2023-07-29 12:03:30 +0900

    작업의 편의성을 위해 JpaRepository로 변경

--- src/main/java/com/example/demo/infrastructure/persistence/product/ProductRepository.java
+++ src/main/java/com/example/demo/infrastructure/persistence/product/ProductRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo.infrastructure.persistence.product;
 
 import com.example.demo.core.product.domain.Product;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends Repository<Product, Long> {
-
-    Product save(Product product);
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    //
 }
--- src/main/java/com/example/demo/infrastructure/persistence/product/StockRepository.java
+++ src/main/java/com/example/demo/infrastructure/persistence/product/StockRepository.java
@@ -1,11 +1,8 @@
 package com.example.demo.infrastructure.persistence.product;
 
 import com.example.demo.core.product.domain.Stock;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface StockRepository extends Repository<Stock, Long> {
-
-    Stock save(Stock stock);
+public interface StockRepository extends JpaRepository<Stock, Long> {
+    //
 }
