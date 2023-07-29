e583ff999be879e107fb1b77980de360b1fe1c23
Junho <chojunho433@gmail.com>
2023-07-29 22:23:09 +0900

    상품조회 Param dto 추가

---
+++ src/main/java/com/example/demo/core/product/param/SearchProductParam.java
@@ -0,0 +1,28 @@
+package com.example.demo.core.product.param;
+
+import com.example.demo.common.enums.product.ProductStatus;
+import lombok.Getter;
+
+@Getter
+public class SearchProductParam {
+
+    private final String productName;
+
+    private final long fromProductAmount;
+
+    private final long toProductAmount;
+
+    private final ProductStatus productStatus;
+
+    public SearchProductParam(
+        String productName,
+        long fromProductAmount,
+        long toProductAmount,
+        ProductStatus productStatus
+    ) {
+        this.productName = productName;
+        this.fromProductAmount = fromProductAmount;
+        this.toProductAmount = toProductAmount;
+        this.productStatus = productStatus;
+    }
+}
