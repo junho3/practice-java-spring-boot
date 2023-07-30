036a262d1e237a138ab92f2c9e48757e7933f169
Junho <chojunho433@gmail.com>
2023-07-30 10:55:31 +0900

    어노테이션 클래스명 변경

--- src/test/java/com/example/demo/annotation/RepositoryJpaTest.java
+++ src/test/java/com/example/demo/annotation/RepositoryJpaTest.java
@@ -11,5 +11,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @DataJpaTest
 @TestEnvironment
-public @interface RepositoryTest {
+public @interface RepositoryJpaTest {
 }
