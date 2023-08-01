22ba6ea85343c666e17744dfcf2bb20a149ec82f
Junho <chojunho433@gmail.com>
2023-08-01 21:25:55 +0900

    비관적 락 조회 메소드 추가

--- src/main/java/com/example/demo/infrastructure/persistence/product/StockRepository.java
+++ src/main/java/com/example/demo/infrastructure/persistence/product/StockRepository.java
@@ -1,11 +1,18 @@
 package com.example.demo.infrastructure.persistence.product;
 
 import com.example.demo.core.product.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     Optional<Stock> findByProductCode(String productCode);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Stock s WHERE s.productCode = :productCode")
+    Optional<Stock> findByProductCodeForUpdate(String productCode);
 }
