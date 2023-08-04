05369de6fe7996cae98b0f108b7e519a38fbe4ec
Junho <chojunho433@gmail.com>
2023-08-04 21:52:58 +0900

    주석 제거

--- src/main/java/com/example/demo/core/product/service/DecreaseStockService.java
+++ src/main/java/com/example/demo/core/product/service/DecreaseStockService.java
@@ -25,14 +25,6 @@ public class DecreaseStockService {
     }
 
     public void decrease(DecreaseStockParam param) {
-        // TODO 판매중인 상품인지 유효성 검사 필요
-        // 여유 재고 >> 0개 미만 떨어질 가능성이 있음.. 5개를 여유 재고로 두고, 5개가 되었을 때 상품 상태를 솔드 아웃으로 변경함
-        // GC를 위해 프리미티브 타입으로 변수 선언
-
-        // 재고 수량은 남아 있어도, 솔드아웃 상태로 변경하는 것
-        // 전시 on / off
-        // 카테고리
-
         param.getStocks()
             .stream()
             .sorted(Comparator.comparing(DecreaseStockParam.Stock::getProductCode))
