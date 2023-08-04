91dac4888605335635ebf5bf3d1cc6b4a1f508fd
Junho <chojunho433@gmail.com>
2023-08-04 22:38:05 +0900

    상품상태에 END 판매종료 추가

--- src/main/java/com/example/demo/common/enums/product/ProductStatus.java
+++ src/main/java/com/example/demo/common/enums/product/ProductStatus.java
@@ -3,8 +3,9 @@ package com.example.demo.common.enums.product;
 public enum ProductStatus {
 
     READY("판매준비"),
-    SELLING("판매"),
-    SOLD_OUT("품절")
+    SELLING("판매중"),
+    SOLD_OUT("품절"),
+    END("판매종료")
     ;
 
     private final String description;
