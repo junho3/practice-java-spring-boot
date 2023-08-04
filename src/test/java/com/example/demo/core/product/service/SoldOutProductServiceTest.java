5d6b51c7916175a91307fdf5a460423f70e8db7e
Junho <chojunho433@gmail.com>
2023-08-04 23:54:07 +0900

    품절 테스트 코드 케이스 추가

--- src/test/java/com/example/demo/core/product/service/SoldOutProductServiceTest.java
+++ src/test/java/com/example/demo/core/product/service/SoldOutProductServiceTest.java
@@ -3,6 +3,8 @@ package com.example.demo.core.product.service;
 import com.example.demo.TestDataInsertSupport;
 import com.example.demo.annotation.IntegrationTest;
 import com.example.demo.common.enums.product.ProductStatus;
+import com.example.demo.common.exceptions.BusinessErrorCode;
+import com.example.demo.common.exceptions.BusinessException;
 import com.example.demo.core.product.domain.Product;
 import com.example.demo.core.product.domain.Stock;
 import com.example.demo.infrastructure.persistence.product.ProductRepository;
@@ -17,6 +19,7 @@ import static com.example.demo.ProductFixtures.PRODUCT_CODE;
 import static com.example.demo.ProductFixtures.PRODUCT_NAME;
 import static com.example.demo.common.enums.product.ProductStatus.SOLD_OUT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @IntegrationTest
 @DisplayName("CreateProductService")
@@ -37,27 +40,71 @@ class SoldOutProductServiceTest extends TestDataInsertSupport {
     @DisplayName("soldOut 메소드는")
     class Describe_soldOut {
 
-        String productCode = PRODUCT_CODE;
+        @Nested
+        @DisplayName("상품 데이터가 존재하지 않는다면")
+        class Context_notFoundData {
+
+            @Test
+            @DisplayName("BusinessException을 던진다.")
+            void it() {
+                BusinessException exception = assertThrows(BusinessException.class, () ->
+                    soldOutProductService.soldOut(PRODUCT_CODE)
+                );
+
+                assertEquals(BusinessErrorCode.NOT_FOUND_PRODUCT, exception.getBusinessErrorCode());
+            }
+        }
 
         @Nested
         @DisplayName("상품 데이터가 존재한다면")
-        class Context_notFoundData {
+        class Context_foundData {
 
-            @BeforeEach
-            void setUp() {
-                final Stock stock = new Stock(productCode, 10_000, 0);
-                save(stock);
-                save(new Product(productCode, PRODUCT_NAME, ProductStatus.SELLING, 100, stock));
+            @Nested
+            @DisplayName("상품 상태가 SELLING 이라면")
+            class Context_selling {
+
+                String productCode = "A202308041152";
+
+                @BeforeEach
+                void setUp () {
+                    final Stock stock = new Stock(productCode, 10_000, 0);
+                    save(stock);
+                    save(new Product(productCode, PRODUCT_NAME, ProductStatus.SELLING, 100, stock));
+                }
+
+                @Test
+                @DisplayName("SOLD_OUT 상태로 변경한다.")
+                void it () {
+                    soldOutProductService.soldOut(productCode);
+
+                    Product product = productRepository.findByProductCode(productCode).get();
+
+                    assertEquals(SOLD_OUT, product.getProductStatus());
+                }
             }
 
-            @Test
-            @DisplayName("SOLD_OUT 상태로 변경한다.")
-            void it() {
-                soldOutProductService.soldOut(productCode);
+            @Nested
+            @DisplayName("상품 상태가 END 라면")
+            class Context_end {
 
-                Product product = productRepository.findByProductCode(productCode).get();
+                String productCode = "A202308041153";
 
-                assertEquals(SOLD_OUT, product.getProductStatus());
+                @BeforeEach
+                void setUp () {
+                    final Stock stock = new Stock(productCode, 10_000, 0);
+                    save(stock);
+                    save(new Product(productCode, PRODUCT_NAME, ProductStatus.END, 100, stock));
+                }
+
+                @Test
+                @DisplayName("BusinessException을 던진다.")
+                void it() {
+                    BusinessException exception = assertThrows(BusinessException.class, () ->
+                        soldOutProductService.soldOut(productCode)
+                    );
+
+                    assertEquals(BusinessErrorCode.NOT_POSSIBLE_CHANGE_END_TO_SOLD_OUT, exception.getBusinessErrorCode());
+                }
             }
         }
     }
