7f9f8b807482553f00bda805654e0ae673d2b2a9
Junho <chojunho433@gmail.com>
2024-11-08 15:32:04 +0900

    MemberNickName1, MemberNickName2 필드 추가

--- src/main/java/com/example/demo/core/member/domain/Member.java
+++ src/main/java/com/example/demo/core/member/domain/Member.java
@@ -1,6 +1,8 @@
 package com.example.demo.core.member.domain;
 
 import com.example.demo.config.persistence.AuditEntity;
+import com.example.demo.infrastructure.persistence.member.MemberNickName1;
+import com.example.demo.infrastructure.persistence.member.MemberNickName2;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,11 +29,19 @@ public class Member extends AuditEntity {
     @Column(name = "member_name", nullable = false, updatable = false)
     private String memberName;
 
+    @Column(name = "member_nickname_1", columnDefinition = "VARCHAR(64)")
+    private MemberNickName1 memberNickName1;
+
+    @Column(name = "member_nickname_2", columnDefinition = "VARCHAR(64)")
+    private MemberNickName2 memberNickName2;
+
     public Member(
         String memberId,
         String memberName
     ) {
         this.memberId = memberId;
         this.memberName = memberName;
+        this.memberNickName1 = new MemberNickName1(memberName);
+        this.memberNickName2 = new MemberNickName2(memberName);
     }
 }
