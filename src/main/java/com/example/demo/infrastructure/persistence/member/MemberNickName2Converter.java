b9902c6707c8e9f6f4c5052978b4b0955a2f5ebe
Junho <chojunho433@gmail.com>
2024-11-08 15:31:42 +0900

    MemberNickName2 추가

---
+++ src/main/java/com/example/demo/infrastructure/persistence/member/MemberNickName2Converter.java
@@ -0,0 +1,17 @@
+package com.example.demo.infrastructure.persistence.member;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class MemberNickName2Converter implements AttributeConverter<MemberNickName2, String> {
+    @Override
+    public String convertToDatabaseColumn(MemberNickName2 memberNickName) {
+        return memberNickName == null ? null : memberNickName.getValue().toLowerCase();
+    }
+
+    @Override
+    public MemberNickName2 convertToEntityAttribute(String s) {
+        return s == null ? null : new MemberNickName2(s);
+    }
+}
