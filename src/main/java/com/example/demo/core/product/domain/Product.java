0f2052334772b5248ebd9fccc304400d88abd351
Junho <chojunho433@gmail.com>
2024-11-24 22:17:13 +0900

    Revert "FoodProduct 분리"
    This reverts commit 30502bca97b16ac7048f2667fd6cdc817454209d.

--- src/main/java/com/example/demo/core/product/domain/Product.java
+++ src/main/java/com/example/demo/core/product/domain/Product.java
@@ -6,7 +6,6 @@ import com.example.demo.common.exceptions.BusinessException;
 import com.example.demo.config.persistence.AuditEntity;
 import com.example.demo.core.stock.domain.Stock;
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -14,8 +13,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -24,12 +21,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "product")
-@DiscriminatorColumn(name = "product_type")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public abstract class Product extends AuditEntity {
+public class Product extends AuditEntity {
     @Id
     @Column(name = "product_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -52,11 +47,13 @@ public abstract class Product extends AuditEntity {
     @JoinColumn(name = "stock_id", referencedColumnName = "stock_id", nullable = false, updatable = false)
     private Stock stock;
 
-    public Product(final String productCode,
-                   final String productName,
-                   final ProductStatus productStatus,
-                   final long productAmount,
-                   final Stock stock) {
+    public Product(
+        String productCode,
+        String productName,
+        ProductStatus productStatus,
+        long productAmount,
+        Stock stock
+    ) {
         this.productCode = productCode;
         this.productName = productName;
         this.productStatus = productStatus;
