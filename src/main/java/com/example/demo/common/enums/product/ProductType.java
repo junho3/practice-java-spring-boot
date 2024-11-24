7aa09662d5ed5e9546f84cd2a80fe1401b8f3108
Junho <chojunho433@gmail.com>
2024-11-24 21:51:29 +0900

    ElectronicProduct 추가

--- src/main/java/com/example/demo/common/enums/product/ProductType.java
+++ src/main/java/com/example/demo/common/enums/product/ProductType.java
@@ -4,9 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum ProductType {
-    FOOD;
+    FOOD, ELECTRONIC;
 
     public static class Values {
         public static final String FOOD = "FOOD";
+        public static final String ELECTRONIC = "ELECTRONIC";
     }
 }
---
+++ src/main/java/com/example/demo/common/enums/product/VoltType.java
@@ -0,0 +1,5 @@
+package com.example.demo.common.enums.product;
+
+public enum VoltType {
+    V220, V110
+}
