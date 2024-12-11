adb71be8362927d3055724bd4a11864502418902
Junho <chojunho433@gmail.com>
2024-12-11 21:07:07 +0900

    validation 어노테이션 추가

--- src/main/java/com/example/demo/web/v1/member/request/CreateMemberRequest.java
+++ src/main/java/com/example/demo/web/v1/member/request/CreateMemberRequest.java
@@ -1,10 +1,13 @@
 package com.example.demo.web.v1.member.request;
 
 import com.example.demo.core.member.param.CreateMemberParam;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 
-public record CreateMemberRequest(String memberId,
-                                  String memberName,
-                                  String email) {
+
+public record CreateMemberRequest(@NotEmpty String memberId,
+                                  @NotEmpty String memberName,
+                                  @Email String email) {
 
     public CreateMemberParam toParam() {
         return new CreateMemberParam(memberId, memberName, email);
