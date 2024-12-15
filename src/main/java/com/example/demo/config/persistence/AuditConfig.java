8825278b7f26d251c492b2aa4266755a9add6506
Junho <chojunho433@gmail.com>
2024-12-15 19:48:05 +0900

    AuditConfig 추가

---
+++ src/main/java/com/example/demo/config/persistence/AuditConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config.persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class AuditConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(LocalDateTime.now());
+    }
+}
