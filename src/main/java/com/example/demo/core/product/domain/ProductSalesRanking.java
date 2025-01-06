4cd9ecd96227ab2bcc31c258dc312f01a9e73e6c
Junho <chojunho433@gmail.com>
2025-01-06 21:43:21 +0900

    엔티티 정의

---
+++ src/main/java/com/example/demo/core/product/domain/ProductSalesRanking.java
@@ -0,0 +1,37 @@
+package com.example.demo.core.product.domain;
+
+import com.example.demo.config.persistence.AuditEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Table(name = "product_sales_ranking")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductSalesRanking extends AuditEntity {
+    @Id
+    @Column(name = "product_sales_ranking_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_code", nullable = false, updatable = false)
+    private String productCode;
+
+    @Column(name = "product_name", nullable = false, updatable = false)
+    private String productName;
+
+    @Column(name = "sales_date", nullable = false, updatable = false)
+    private LocalDate salesDate;
+
+    @Column(name = "sales_quantity", nullable = false)
+    private long salesQuantity;
+}
