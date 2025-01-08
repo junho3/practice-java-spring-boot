40a1a00f06f5a3d4110a89a47c38ccb2eb9dd621
Junho <chojunho433@gmail.com>
2025-01-08 23:02:43 +0900

    캐시 조회 메소드 추가

--- src/main/java/com/example/demo/core/product/service/SearchProductService.java
+++ src/main/java/com/example/demo/core/product/service/SearchProductService.java
@@ -5,6 +5,7 @@ import com.example.demo.core.product.param.SearchProductParam;
 import com.example.demo.core.product.result.SearchProductResult;
 import com.example.demo.infrastructure.persistence.product.ProductCustomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,13 @@ public class SearchProductService {
 
     public SearchProductResult search(SearchProductParam param) {
         Page<Product> products = productCustomRepository.search(param);
+
+        return SearchProductResult.from(products);
+    }
+
+    @Cacheable(value = "products", key = "#param.productStatus")
+    public SearchProductResult searchWithCache(final SearchProductParam param) {
+        final Page<Product> products = productCustomRepository.search(param);
 
         return SearchProductResult.from(products);
     }
