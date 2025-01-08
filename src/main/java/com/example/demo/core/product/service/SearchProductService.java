e01132f954533a297288a539be767471209b11d9
Junho <chojunho433@gmail.com>
2025-01-08 23:42:26 +0900

    캐시 키 변경

--- src/main/java/com/example/demo/core/product/service/SearchProductService.java
+++ src/main/java/com/example/demo/core/product/service/SearchProductService.java
@@ -23,7 +23,7 @@ public class SearchProductService {
         return SearchProductResult.from(products);
     }
 
-    @Cacheable(value = "products", key = "#param.pageable")
+    @Cacheable(value = "products", key = "#param")
     public SearchProductResult searchWithCache(final SearchProductParam param) {
         final Page<Product> products = productCustomRepository.search(param);
 
