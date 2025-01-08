76b7e49c73a0067b48250d6b641a7665442d1cd7
Junho <chojunho433@gmail.com>
2025-01-08 23:20:59 +0900

    @RequiredArgsConstructor로 교체

--- src/main/java/com/example/demo/web/v1/product/SearchProductController.java
+++ src/main/java/com/example/demo/web/v1/product/SearchProductController.java
@@ -6,19 +6,17 @@ import com.example.demo.web.ApiResponse;
 import com.example.demo.web.v1.product.request.SearchProductRequest;
 import com.example.demo.web.v1.product.response.SearchProductResponse;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SearchProductController {
 
     private final SearchProductService searchProductService;
-
-    public SearchProductController(SearchProductService searchProductService) {
-        this.searchProductService = searchProductService;
-    }
 
     @GetMapping("/v1/products")
     public ApiResponse<SearchProductResponse> search(@RequestParam(required = false, defaultValue = "true") boolean cache,
