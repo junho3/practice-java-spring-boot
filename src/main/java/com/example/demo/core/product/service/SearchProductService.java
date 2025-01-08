1b1dc81f8d7a9764f2f801af023198986862b6c3
Junho <chojunho433@gmail.com>
2025-01-08 22:05:38 +0900

    @RequiredArgsConstructor 추가

--- src/main/java/com/example/demo/core/product/service/SearchProductService.java
+++ src/main/java/com/example/demo/core/product/service/SearchProductService.java
@@ -4,19 +4,17 @@ import com.example.demo.core.product.domain.Product;
 import com.example.demo.core.product.param.SearchProductParam;
 import com.example.demo.core.product.result.SearchProductResult;
 import com.example.demo.infrastructure.persistence.product.ProductCustomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional(readOnly = true)
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class SearchProductService {
 
     private final ProductCustomRepository productCustomRepository;
-
-    public SearchProductService(ProductCustomRepository productCustomRepository) {
-        this.productCustomRepository = productCustomRepository;
-    }
 
     public SearchProductResult search(SearchProductParam param) {
         Page<Product> products = productCustomRepository.search(param);
