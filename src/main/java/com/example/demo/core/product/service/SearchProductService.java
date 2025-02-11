e2cadb434152141c22d1a616f08daeb20272a32b
junho3 <chojunho433@gmail.com>
2025-02-11 21:07:04 +0900

    ProductRepository로 주입하도록 변경

--- src/main/java/com/example/demo/core/product/service/SearchProductService.java
+++ src/main/java/com/example/demo/core/product/service/SearchProductService.java
@@ -4,7 +4,7 @@ import com.example.demo.config.persistence.ReadTransactional;
 import com.example.demo.core.product.domain.Product;
 import com.example.demo.core.product.param.SearchProductParam;
 import com.example.demo.core.product.result.SearchProductResult;
-import com.example.demo.infrastructure.persistence.product.ProductRepositoryCustom;
+import com.example.demo.infrastructure.persistence.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
@@ -15,17 +15,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SearchProductService {
 
-    private final ProductRepositoryCustom productRepositoryCustom;
+    private final ProductRepository productRepository;
 
     public SearchProductResult search(final SearchProductParam param) {
-        final Page<Product> products = productRepositoryCustom.search(param);
+        final Page<Product> products = productRepository.search(param);
 
         return SearchProductResult.from(products);
     }
 
     @Cacheable(value = "products", key = "#param")
     public SearchProductResult searchWithCache(final SearchProductParam param) {
-        final Page<Product> products = productRepositoryCustom.search(param);
+        final Page<Product> products = productRepository.search(param);
 
         return SearchProductResult.from(products);
     }
