5d9ba9e1e096bf67fe3cb6825d7cc4a8d7f7f2b7
junho3 <chojunho433@gmail.com>
2025-02-11 21:03:14 +0900

    생성자 loombok으로 대체

--- src/main/java/com/example/demo/infrastructure/persistence/product/ProductRepositoryImpl.java
+++ src/main/java/com/example/demo/infrastructure/persistence/product/ProductRepositoryImpl.java
@@ -7,16 +7,14 @@ import com.example.demo.core.product.param.SearchProductParam;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
+@RequiredArgsConstructor
 public class ProductRepositoryImpl implements ProductRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
-
-    public ProductRepositoryImpl(JPAQueryFactory queryFactory) {
-        this.queryFactory = queryFactory;
-    }
 
     @Override
     public Page<Product> search(final SearchProductParam param) {
