6676d7b6eb52682ac847dde6e41ef49fd729e126
junho3 <chojunho433@gmail.com>
2025-03-10 22:54:01 +0900

    record로 변환

--- src/main/java/com/example/demo/web/v1/order/request/CreateOrderRequest.java
+++ src/main/java/com/example/demo/web/v1/order/request/CreateOrderRequest.java
@@ -1,43 +1,25 @@
 package com.example.demo.web.v1.order.request;
 
 import com.example.demo.core.order.param.CreateOrderParam;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class CreateOrderRequest {
+public record CreateOrderRequest(@NotNull long memberNo,
+                                 @Valid @NotEmpty Set<Product> products) {
 
-    @NotNull
-    private long memberNo;
-
-    @NotEmpty
-    private Set<Product> products;
-
-    @Getter
-    public static class Product {
-
-        @NotEmpty
-        private String productCode;
-
-        @NotEmpty
-        private String productName;
-
-        @Positive
-        private long quantity;
-
-        @Positive
-        private long productAmount;
+    public record Product(@NotEmpty String productCode,
+                          @NotEmpty String productName,
+                          @Positive long quantity,
+                          @Positive BigDecimal productAmount) {
 
         public CreateOrderParam.Product toParam() {
-            return new CreateOrderParam.Product(productCode, productName, quantity, productAmount);
+            return new CreateOrderParam.Product(productCode, productName, quantity, productAmount.longValue());
         }
     }
 
