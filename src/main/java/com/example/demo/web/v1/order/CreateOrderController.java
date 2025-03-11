5cdf717c23f37e89773446b6cab221f631a52d0d
junho3 <chojunho433@gmail.com>
2025-03-11 22:49:59 +0900

    @RequiredArgsConstructor로 교체

--- src/main/java/com/example/demo/web/v1/order/CreateOrderController.java
+++ src/main/java/com/example/demo/web/v1/order/CreateOrderController.java
@@ -6,18 +6,16 @@ import com.example.demo.web.ApiResponse;
 import com.example.demo.web.v1.order.request.CreateOrderRequest;
 import com.example.demo.web.v1.order.response.CreateOrderResponse;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CreateOrderController {
 
     private final CreateOrderService createOrderService;
-
-    public CreateOrderController(CreateOrderService createOrderService) {
-        this.createOrderService = createOrderService;
-    }
 
     @PostMapping("/v1/order")
     public ApiResponse<CreateOrderResponse> create(@RequestBody @Valid CreateOrderRequest request) {
