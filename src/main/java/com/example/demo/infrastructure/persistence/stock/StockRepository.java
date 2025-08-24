6985b28e30dcf464ec0132cbc987ee66d3dd9d4c
junho3 <chojunho433@gmail.com>
2025-08-24 23:16:48 +0900

    재고차감 Native Query 메소드 추가

--- src/main/java/com/example/demo/infrastructure/persistence/stock/StockRepository.java
+++ src/main/java/com/example/demo/infrastructure/persistence/stock/StockRepository.java
@@ -4,7 +4,9 @@ import com.example.demo.core.stock.domain.Stock;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -15,4 +17,17 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT s FROM Stock s WHERE s.productCode = :productCode")
     Optional<Stock> findByProductCodeForUpdate(String productCode);
+
+    @Modifying
+    @Query(
+        value =
+            " UPDATE stock SET quantity = (quantity - :quantity) " +
+            " WHERE product_code = :productCode " +
+            " AND quantity >= :quantity ",
+        nativeQuery = true
+    )
+    int decreaseQuantity(
+        @Param("productCode") String productCode,
+        @Param("quantity") long quantity
+    );
 }
