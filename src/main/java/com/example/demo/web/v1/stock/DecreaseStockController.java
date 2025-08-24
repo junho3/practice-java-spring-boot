d668e6d01bb2b81b98ac9751ad9a2785c35aaff8
junho3 <chojunho433@gmail.com>
2025-08-24 12:38:18 +0900

    dto를 record로 변경 @WebMvcTest 테스트 코드 추가

--- src/main/java/com/example/demo/web/v1/stock/DecreaseStockController.java
+++ src/main/java/com/example/demo/web/v1/stock/DecreaseStockController.java
@@ -4,18 +4,16 @@ import com.example.demo.core.stock.service.DecreaseStockService;
 import com.example.demo.web.ApiResponse;
 import com.example.demo.web.v1.stock.request.DecreaseStockRequest;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class DecreaseStockController {
 
     private final DecreaseStockService decreaseStockService;
-
-    public DecreaseStockController(DecreaseStockService decreaseStockService) {
-        this.decreaseStockService = decreaseStockService;
-    }
 
     @PostMapping("/v1/stocks/decrease")
     public ApiResponse<Void> decrease(@RequestBody @Valid DecreaseStockRequest request) {
